786f449e0b875c9af16273ddbb3ec043cf4f8aa7
Ellahi <ellahi@mbp.lan>
2023-03-03 11:44:32 -0500

    explore page coming together

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -1,25 +1,36 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './css/Explore.css'
 import glassbox1 from './img/glassbox1.png'
+import glassbox2 from './img/glassbox2.png'
+import ball1 from './img/ball1.png'
 
+const navStyle = {textDecoration: "none", color: "white", backgroundColor: "transparent", border: "none"};
 
 function Explore() {
   return (
-    <div className='main'>
+    <div className='explore-page'>
+        <video src='/HomePageBG2.mp4' autoPlay loop muted className='explore-vid'/> 
         <h1 className='explore-header'>choose your destination</h1>
         <div className='boxes'>
             <div className='box'>
                 <div>
-                    <img src={glassbox1} alt="glassbox 1" />
-                        <p>Location One</p>
+                    <Link to="/tbd">
+                        <img src={glassbox1} alt="glassbox 1" />
+                            <p><Link style={navStyle}>Location One</Link></p>
+                    </Link>
                 </div>
                 <div>
-                    <img src={glassbox1} alt="glassbox 2" />
-                        <p>Location Two</p>
+                    <Link to="/tbd">
+                        <img src={ball1} alt="ball 1" />
+                            <p><Link style={navStyle}>Location Two</Link></p>
+                    </Link>                
                 </div>
                 <div>
-                    <img src={glassbox1} alt="glassbox 3" />
-                        <p>Location Three</p>
+                    <Link to="/tbd">
+                        <img src={glassbox2} alt="glassbox 2" />
+                            <p><Link style={navStyle}>Location Three</Link></p>
+                    </Link>                
                 </div>
             </div>
         </div>
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,5 +1,5 @@
 import './css/Homepage.css';
-import { Routes, Route, Outlet, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 const navStyle = {textDecoration: "none", color: "white"};
@@ -7,7 +7,7 @@ const navStyle = {textDecoration: "none", color: "white"};
 function Homepage() {
   return (
     <div className="content">
-      <video src='/HomePageBG.mp4' autoPlay loop muted/> 
+      <video src='/HomePageBG.mp4' autoPlay loop muted className='homepage-vid'/> 
       <div className="header">
       <h1>welcome to the tbd</h1>
       <h3> <Link style={navStyle} to = "/explore"> click here to explore</Link></h3>
